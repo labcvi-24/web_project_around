c6dee2f000ea9681f7d420437a76d6be6a4acd71
luisandra <labcvi24@gmail.com>
2025-05-29 13:33:50 +0200

    utimos arreglos

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
-const content = document.querySelector(".content");
-
+const content = document.querySelector(".content")
 const profileButton = content.querySelector(".profile__button-edit")
-
-
 const popupClose = content.querySelector(".popup__button-close")
-
-
 const popup = content.querySelector(".popup")
-
-
 const popupForm = content.querySelector(".popup__form")
 
+
+function handleOpenPopup(){
+    popup.classList.add("popup__opened");
+}
+profileButton.addEventListener("click", handleOpenPopup);
+
+
+function handleClosePopup(){
+    popup.classList.remove("popup__opened");
+}
+popupClose.addEventListener("click",handleClosePopup)
 
 
 function handleProfileFormSubmit(evt){
@@ -24,18 +28,8 @@ function handleProfileFormSubmit(evt){
 
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
+
+    handleClosePopup();
 }
+
 popupForm.addEventListener('submit', handleProfileFormSubmit);
-
-
-function handleOpenPopup(){
-    popup.classList.add("popup_opened");
-
-}
-profileButton.addEventListener("click", handleOpenPopup);
-
-function handleClosePopup(){
-    popup.classList.remove("popup_opened");
-}
-popupClose.addEventListener("click",handleClosePopup)
-
