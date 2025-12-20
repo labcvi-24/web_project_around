aef2a21e8ab00c57791508d2ecfb401e33496bf9
luisandra <labcvi24@gmail.com>
2025-12-20 10:37:20 +0100

    carpeta final components

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-import Card from "../pages/Card.js";
+import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
-import PopupWithForm from "../pages/PopupWithForm.js";
-import PopupWithImage from "../pages/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
 
   const settings = {
